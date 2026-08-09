8706cd0f3e25e72019cd9c8647990118731cb042
THANMAY12 <146874256+THANMAY12@users.noreply.github.com>
2026-08-09 12:19:50 +0530

    Add: majority-element - submission-3

---
+++ Data Structures & Algorithms/majority-element/submission-3.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int e=0;
+        int c=0;
+        for(int i=0;i<nums.length;i++){
+            if(c==0){
+                e=nums[i];
+                c++;
+                
+            }
+            else if(e!=nums[i]) c--;
+            else {
+                c++;
+            }
+            System.out.println(e+" "+c);
+        }
+        return e;
+    }
+}
