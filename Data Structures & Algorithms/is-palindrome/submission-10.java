04608ba915c0b27fa9b167eb1b87682a119e89c3
THANMAY12 <146874256+THANMAY12@users.noreply.github.com>
2026-08-09 15:57:04 +0530

    Add: is-palindrome - submission-10

---
+++ Data Structures & Algorithms/is-palindrome/submission-10.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int l=0;
+        
+        s=s.toLowerCase();
+        int r=s.length()-1;
+
+        while(l<r){
+            if(!Character.isLetterOrDigit(s.charAt(l))) l++;
+            else if(!Character.isLetterOrDigit(s.charAt(r))) r--;
+            else if(s.charAt(l)!=s.charAt(r)) {
+                return false;
+            }else{
+            l++;
+            r--;}
+        }
+        return true;
+    }
+}
