54f043a1b4a864ddb381b1f8fc0b517edc4e9420
THANMAY12 <146874256+THANMAY12@users.noreply.github.com>
2026-08-09 14:36:52 +0530

    Add: next-greater-element-i - submission-3

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-3.java
@@ -0,0 +1,19 @@
+class Solution {
+    static int check(int[] nums2,int n,int j){
+        for(int i=j+1;i<nums2.length;i++){
+            System.out.println(nums2[i]+" "+j+" "+n);
+            if(nums2[i]>n) return nums2[i];
+        }
+        return -1;
+    }
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int arr[]=new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            for(int j=0;j<nums2.length;j++){
+                if(nums1[i]==nums2[j]){ arr[i]=check(nums2,nums2[j],j);
+                break;}
+            }
+        }
+        return arr;
+    }
+}
